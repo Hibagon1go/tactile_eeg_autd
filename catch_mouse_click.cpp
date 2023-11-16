f6e77a7b26f0feebac9ccee5cbfb0066a6694998
Hibagon1go <hibarayoichiro871@gmail.com>
2023-11-16 16:53:11 +0900

    catch mouse click script added @11/16

--- catch_mouse_click.cpp
+++ catch_mouse_click.cpp
@@ -10,10 +10,27 @@
 #include <chrono>
 #include <thread>
 #include <random>
+#include <fstream>
+#include <ctime>
+
 using std::this_thread::sleep_for;
+
+// cl /EHsc catch_mouse_click.cpp /link user32.lib
 
 HWND hwndMessageWindow;
 HANDLE hComm;
+std::ofstream file;
+
+void write_timestamp(std::ofstream& file) {
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+
+    char buffer[32];
+    if (std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &tm)) {
+        file << buffer << std::endl;  // CSVに書き込む形式を指定
+    }
+}
+
 LRESULT CALLBACK MouseWindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     std::string dataToSend0 = "00";
@@ -30,13 +47,11 @@ LRESULT CALLBACK MouseWindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM
             std::unique_ptr<BYTE[]> rawdata = std::make_unique<BYTE[]>(dwSize);
             if (GetRawInputData((HRAWINPUT)lParam, RID_INPUT, rawdata.get(), &dwSize, sizeof(RAWINPUTHEADER)) == dwSize)
             {
-                RAWINPUT* raw = reinterpret_cast<RAWINPUT*>(rawdata.get());
-                if (raw->header.dwType == RIM_TYPEMOUSE && raw->data.mouse.usButtonFlags & RI_MOUSE_LEFT_BUTTON_DOWN)
+                RAWINPUT *raw = reinterpret_cast<RAWINPUT *>(rawdata.get());
+                if (raw->header.dwType == RIM_TYPEMOUSE && raw->data.mouse.usButtonFlags & RI_MOUSE_RIGHT_BUTTON_DOWN)
                 {
-                    WriteFile(hComm, dataToSend9.c_str(), dataToSend0.size(), &bytesWritten, NULL);
-                    sleep_for(std::chrono::milliseconds(10));
-                    WriteFile(hComm, dataToSend0.c_str(), dataToSend0.size(), &bytesWritten, NULL);
-                    std::cout << "Left mouse button down (Raw Input)." << std::endl;
+                    write_timestamp(file);
+                    std::cout << "Right mouse button down (Raw Input)." << std::endl;
                 }
             }
         }
@@ -73,41 +88,13 @@ void RegisterRawInputDevices(HWND hwnd)
 
 int main()
 {
-    // シリアルポートを開く
-    HANDLE hComm;
-    hComm = CreateFile("COM3", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-    if (hComm == INVALID_HANDLE_VALUE)
-    {
-        if (GetLastError() == ERROR_FILE_NOT_FOUND)
-        {
-            printf("Serial port does not exist.\n");
-        }
-        printf("Failed to open serial port.\n");
-    }
-    else
-    {
-        printf("COM3 port found.\n");
+    file.open("C:/Users/user/Desktop/master_thesis_data/1116/timestamp6.csv", std::ios::out | std::ios::app);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open the CSV file." << std::endl;
+        return -1;
     }
 
-    // COMポートの設定
-    DCB dcbSerialParams = { 0 };
-    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-    GetCommState(hComm, &dcbSerialParams);
-    dcbSerialParams.BaudRate = CBR_115200;
-    dcbSerialParams.ByteSize = 8;
-    dcbSerialParams.StopBits = ONESTOPBIT;
-    dcbSerialParams.Parity = NOPARITY;
-    SetCommState(hComm, &dcbSerialParams);
-
-    // トリガーの用意
-    std::string dataToSendRR = "RR";
-    DWORD bytesWritten;
-
-    // BrainAmpではRR2回送る
-    WriteFile(hComm, dataToSendRR.c_str(), dataToSendRR.size(), &bytesWritten, NULL);
-    WriteFile(hComm, dataToSendRR.c_str(), dataToSendRR.size(), &bytesWritten, NULL);
-
-    WNDCLASS wndClass = { 0 };
+    WNDCLASS wndClass = {0};
     wndClass.lpfnWndProc = MouseWindowProcedure;
     wndClass.hInstance = GetModuleHandle(NULL);
     wndClass.lpszClassName = "MouseMessageWindowClass";
